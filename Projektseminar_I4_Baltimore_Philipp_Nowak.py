28046e97a106dac5cb03a92ff783e795626df150
philipp.nowak@uni-duesseldorf.de <philipp.nowak@hhu.de>
2016-09-14 23:57:57 +0200

    Functions sum and average

--- Projektseminar_I4_Baltimore_Philipp_Nowak.py
+++ Projektseminar_I4_Baltimore_Philipp_Nowak.py
@@ -4,14 +4,32 @@
 import csv
 
 
+#Im Folgenden befinden sich Funktionen, die für die (grafische) Auswertung benötigt werden
+#Funktion 1: Summe einer Liste berechnen
+def sum_function(my_list):
+	list_sum = 0
+	for i in my_list:
+		list_sum += i
+	return list_sum
+	
+#Funktion 2: Durchschnitt berechnen
+def average_function(my_list):
+	list_sum = 0
+	for i in my_list:
+		list_sum += i
+	return list_sum/len(my_list)
+
+
 #Listen definieren
 baltimore_complete = []
+
 
 #CSV-Datei öffnen & einlesen
 with open ('Crime___Safety__2010-2014_.csv', 'rU') as csv_input:
 	reader = csv.reader(csv_input, delimiter=',')
 	for row in reader:
 		baltimore_complete.append(row)
+
 
 #test
 print baltimore_complete
